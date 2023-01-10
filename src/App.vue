59fe3575be10c1b69995736c6b7f9d7955b90e66
onezeun <onezeun@gmail.com>
2023-01-10 12:41:45 +0900

    feat: vuetify 설치

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,18 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <v-app>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+
 export default {
-  name: "App",
+  name: 'App',
 
   data: () => ({
     //
   }),
 };
 </script>
-
-<style>
-@font-face {
-  font-family: "KOTRAHOPE";
-  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2110@1.0/KOTRAHOPE.woff2")
-    format("woff2");
-  font-weight: normal;
-  font-style: normal;
-}
-
-html {
-  font-size: 10px;
-}
-
-#app {
-  font-family: "KOTRAHOPE";
-  max-width: 760px;
-  margin: auto;
-}
-</style>
